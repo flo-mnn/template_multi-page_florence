c3c95020bd861444606fa38c02acc87219cf9d98
Florence <monnaie.florence@gmail.com>
2021-01-30 19:30:53 +0100

    done

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     public function index(){
-        return view('pages.about');
+        $about=
+            (object)['title'=>'Strong Coffee, Strong Roots','subtitle'=>'About Our Cafe','text'=>['Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.',['We guarantee that you will fall in',' lust ','with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.']]]
+        ;
+        return view('pages.about',compact('about'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [AboutController::class, 'index'])->name('about');
+Route::get('/products', [ProductsController::class, 'index'])->name('products');
+Route::get('/store', [StoreController::class, 'index'])->name('store');
