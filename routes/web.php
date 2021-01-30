20d53d94e6f502966678431a5363beb334ca07dd
Florence <monnaie.florence@gmail.com>
2021-01-30 19:32:23 +0100

    done

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/products', [ProductsController::class, 'index'])->name('products');
